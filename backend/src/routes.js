72e88106c73bb3f9bcc5da75185e35e510ebfa15
cristianfreire <cristianpassos51@gmail.com>
2020-04-24 17:59:52 -0400

    first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,11 @@
+const routes = require('express').Router()
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+
+routes.post('/posts', multer(multerConfig).single('file'), (req,res) =>{
+    console.log(req.file)
+
+    res.json({hello: 'World'})
+})
+
+module.exports = routes
